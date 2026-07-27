b1559499c6035c4824854b01e72dc8dd6d8a4ca1
meow-wwww <wangxiaoyu2001@pku.edu.cn>
2026-07-27 16:25:46 +0800

    comment out unused pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
-import Reading from "./pages/Reading.tsx";
+// import Reading from "./pages/Reading.tsx";
 import Crochet from "./pages/Crochet.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
